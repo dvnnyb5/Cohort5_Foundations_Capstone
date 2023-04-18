dbbc36f3e3fbdc6c2d6906e593e48ccd2024a8bc
dvnnyb5 <125165599+dvnnyb5@users.noreply.github.com>
2023-04-18 11:58:40 -0600

    still working

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-
+import os
 import sqlite3
 from user import User, Manager
 connection = sqlite3.connect('capstone.db')
 cursor = connection.cursor()
 
 def user_menu():
+    # os.system('cls' if os.name == 'nt' else 'clear')
     return input("""
-    ******MENU******
+    ******MAIN MENU******
+
     [1] VIEW ASSESSMENT DATA
     [2] EDIT NAME
     [3] EDIT PASSWORD
     [4] LOG OUT
         """)
 def admin_menu():
+    # os.system('cls' if os.name == 'nt' else 'clear')
     return input("""
-    ******MENU******
+    ******MAIN MENU******
+
     [1] VIEW ALL USERS
     [2] SEARCH FOR USER
     [3] VIEW USER REPORTS/COMPETENCY LEVELS
@@ -22,9 +26,33 @@ def admin_menu():
     [5] VIEW LIST OF ASSESSMENTS
     [6] ADD
     [7] EDIT
-    [8] DELETE
+    [8] DELETE ASSESSMENT RESULT
     [9] LOG OUT
         """)
+def add_menu():
+    # os.system('cls' if os.name == 'nt' else 'clear')
+    return input("""
+    ******ADD MENU******
+
+    [1] ADD NEW USER
+    [2] ADD NEW COMPETENCY
+    [3] ADD ADD NEW ASSESSMENT TO COMPETENCY
+    [4] ADD AN ASSESSMENT RESULT FOR A USER FOR AN ASSESSMENT
+    [5] BACK TO MAIN MENU
+        """)
+def edit_menu():
+    # os.system('cls' if os.name == 'nt' else 'clear')
+    return input("""
+    ******EDIT MENU******
+
+    [1] EDIT A USERS INFORMATION
+    [2] EDIT A COMPETENCY
+    [3] EDIT AN ASSESSMENT
+    [4] EDIT AN ASSESSMENT RESULT
+    [5] BACK TO ORIGINAL MENU
+        """)
+
+
     
 user = None
 
@@ -38,7 +66,7 @@ while not user:
         user = potential_user
         print("\n\nlogin successful\n\n")
     else:
-        print("\nTry Again\nincorrect password\n\n")
+        print("\nTry Again\nIncorrect Username or Password!\n\n")
 
 instantiated_user = None
 if user[6] == 1:
@@ -55,10 +83,48 @@ while instantiated_user.is_manager == 1:
         instantiated_user.view_users()
     elif selection == '2':
         instantiated_user.search_user()
+    elif selection == '3':
+        instantiated_user.report_of_users()
+    elif selection == '4':
+        instantiated_user.report_of_single_user()
     elif selection == '5':
-        instantiated_user.get_all_assessments()
-    else:
+        instantiated_user.report_of_single_user()
+    elif selection == '6':
+        while True:
+            selection2 = add_menu()
+            if selection2 == '1':
+                instantiated_user.add_user()
+            elif selection2 == '2':
+                instantiated_user.add_new_competency()
+            elif selection2 == '3':
+                instantiated_user.add_new_assessment()
+            elif selection2 == '4':
+                instantiated_user.add_assessment_result()
+            elif selection2 == '5':
+                break
+            else:
+                print("/nTry a valid selection!")
+    elif selection == '7':
+         while True:
+            selection3 = edit_menu()
+            if selection3 == '1':
+                pass
+            elif selection3 == '2':
+                pass
+            elif selection3 == '3':
+                pass
+            elif selection3 == '4':
+                pass
+            elif selection3 == '5':
+                break
+            else:
+                print("/nTry a valid selection!")
+    elif selection == '8':
+        pass
+    elif selection == '9':
         break
+    else:
+        print('Try a valid selection')
 
 # user engine
 while instantiated_user.is_manager != 1:
@@ -71,6 +137,3 @@ while instantiated_user.is_manager != 1:
         instantiated_user.change_password()
     else:
         break
-
-
-new_user = Manager(user[0], user[1], user[2], user[3], user[4])
--- user.py
+++ user.py
@@ -1,6 +1,8 @@
 import sqlite3
+from datetime import datetime
 connection = sqlite3.connect("capstone.db")
 cursor = connection.cursor()
+
 
 class User:
     def __init__(self,user_id,first, last, phone, email, password):
@@ -56,19 +58,48 @@ class Manager(User):
         for row in rows:
             print(f"{row[0]:<20}{row[1]:<20}{row[2]:<20}{row[3]:<20}{row[4]:<20}{row[5]:<20}{row[6]:<20}{row[7]}")
 
+    def view_competencies(self):
+        view_query = '''SELECT * FROM Competencies'''
+        data = cursor.execute(view_query,).fetchall()
+        print(f"\n{'Competency ID':<20}{'Competency Name':<20}{'Date Created':<20}")
+        print(f"{'--------------':<20}{'----------------':<20}{'-------------':<20}")
+        for line in data:
+            print(f"{line[0]:<20}{line[1]:<20}{line[2]:<20}")
+
     def report_of_users(self):
-        all_users_query = '''SELECT FROM Competencies'''
-        results = cursor.execute(all_users_query,).fetchall()
-        print(f"\n{'User ID':<20}{'First Name':<20}{'Last Name':<20}")
-        print(f"{'---------':<20}{'----------':<20}{'---------':<20}")
+        all_users_query = '''
+            SELECT U.user_id, U.first, U.last, C.competency_name, COALESCE(A.result, NULL) AS result
+            FROM Users U
+            LEFT OUTER JOIN (
+                SELECT user_id, result, competency_id
+                FROM Assessments
+                WHERE competency_id = ?
+            ) A ON U.user_id = A.user_id
+            LEFT OUTER JOIN Competencies C ON C.competency_id = A.competency_id
+            WHERE U.user_id NOT IN (SELECT user_id FROM Assessments)
+            OR A.user_id IS NOT NULL
+        '''
+        print("\n\n")
+        self.view_competencies()
+        print("\n\n")
+        search = input('Competency id: ')
+        results = cursor.execute(all_users_query,(search,)).fetchall()
+        print(f"\n{'User ID':<20}{'First Name':<20}{'Last Name':<20}{'Subject':<20}{'Score'}")
+        print(f"{'-------':<20}{'----------':<20}{'---------':<20}{'-------':<20}{'-----'}")
         for row in results:
-            print(f"{row[0]:<20}{row[1]:<20}{row[2]:<20}")
-
-    def report_of_user(self):
-        query = '''SELECT'''
-
-    def view_list_of_assessments_for_a_given_user(self):
-        pass
+            if row[3] == None:
+                print(f"{row[0]:<20}{row[1]:<20}{row[2]:<20}{'NONE':<20}{'NONE':<20}")
+            else:
+                print(f"{row[0]:<20}{row[1]:<20}{row[2]:<20}{row[3]:<20}{row[4]:<20}")
+    
+    def report_of_single_user(self):
+        query = '''SELECT U.user_id, U.first, U.last, C.competency_name, A.result FROM Users U JOIN Assessments A ON A.user_id=U.user_id JOIN Competencies C ON C.competency_id=A.competency_id WHERE U.user_id = ?'''
+        user_id = input('Enter user id ')
+        results = cursor.execute(query,(user_id,))
+        print(f"\n{'User ID':<20}{'First Name':<20}{'Last Name':<20}{'Subject':<20}{'Score'}")
+        print(f"{'-------':<20}{'----------':<20}{'---------':<20}{'-------':<20}{'-----'}")
+        for row in results:
+            print(f"{row[0]:<20}{row[1]:<20}{row[2]:<20}{row[3]:<20}{row[4]:<20}")
 
     def add_user(self):
         create_query = '''INSERT INTO Users (first, last, email, phone, password, is_manager)
@@ -83,23 +114,34 @@ class Manager(User):
         connection.commit()
     
     def add_new_competency(self):
-        pass
+        query = '''INSERT INTO Competencies (competency_name, date_created)
+        VALUES(?,?)'''
+        name = input('Enter new competency name ')
+        date_created = datetime.now()
+        cursor.execute(query,(name,date_created))
+        connection.commit()
+        print(f'{name} has been added!')
 
     def add_new_assessment(self):
-        pass
+        query = '''INSERT INTO Assessments (user_id, competency_id)
+        VALUES(?,?)'''
+        user_id = input('Enter user_id ')
+        competency_id = input('Enter competency_id ')
+        cursor.execute(query,(user_id, competency_id))
+        connection.commit()
 
     def add_assessment_result(self):
-        pass
+        query = '''UPDATE Assessments SET result = ? WHERE assessment_id = ?'''
+        assessment_id = input('Enter assessment_id ')
+        result = input('Enter result ')
+        cursor.execute(query,(result, assessment_id))
+        connection.commit()
+        print('Result recorded!')
 
-    def get_all_assessments(self):
+    def all_assessments_given_user(self):
         view_query = '''SELECT * FROM Assessments'''
         data = cursor.execute(view_query).fetchall()
         for line in data:
             print(line)
-
-
-# user1 = Manager(1, 'sdfkljasd', 'a;sldkfjasld', 's;adlfsd', 'sdkflaskd', 'sd;lfasdke')
-# user1.change_password()
-
-
-
+    
+    
